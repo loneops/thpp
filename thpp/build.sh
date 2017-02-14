b48c3941e298f20010923018c1e93929183eb080
Mayank <mayank.pundir@reverieinc.com>
2017-02-14 23:55:46 +0530

    Update build & cmakelists with current googletest zip

--- thpp/build.sh
+++ thpp/build.sh
@@ -15,14 +15,9 @@ if [[ ! -r ./Tensor.h ]]; then
   exit 1
 fi
 
-rm -rf gtest-1.7.0 gtest-1.7.0.zip
-curl -JLO https://googletest.googlecode.com/files/gtest-1.7.0.zip
-if [[ $(sha1sum -b gtest-1.7.0.zip | cut -d' ' -f1) != \
-      'f85f6d2481e2c6c4a18539e391aa4ea8ab0394af' ]]; then
-  echo "Invalid gtest-1.7.0.zip file" >&2
-  exit 1
-fi
-unzip gtest-1.7.0.zip
+rm -rf googletest-release-1.7.0 googletest-release-1.7.0.zip
+curl -JLOk https://github.com/google/googletest/archive/release-1.7.0.zip
+unzip googletest-release-1.7.0.zip
 
 # Build in a separate directory
 mkdir -p build
